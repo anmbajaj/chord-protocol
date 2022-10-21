627c86ffd77245ea7296eaddd2800165631681a0
Anmol Bajaj <anmbajaj@macOS.local>
2022-10-21 03:37:35 -0400

    Added states to the chord actors and moved SHA computation logic to actors

--- chord_node.erl
+++ chord_node.erl
@@ -10,10 +10,59 @@
 -author("anmbajaj").
 
 %% API
--export([start/0]).
+-export([start/1, init/0]).
 
-start() ->
+-define(NUMBER_OF_BITS, 256).
+
+-record(state, {
+  id,
+  predecessor,
+  successor,
+  finger_table
+}).
+
+start(#state{
+  id = HashValue,
+  predecessor = Predecessor,
+  successor = Successor,
+  finger_table = FingerTable
+} = State) ->
+  io:fwrite("Created chord node ~p with state ~p", [self(), State]),
   receive
-    {abc} -> ok
-  end,
-  start().
+    {abc} ->
+      _ = HashValue,
+      _ = Predecessor,
+      _ = Successor,
+      _ = FingerTable,
+      _ = State;
+    {Sender, add_new_node, Node} ->
+      io:fwrite("Received Message: Add new node ~p to the network ~n", [Node]),
+      Message = "New node successfully added to the network",
+      Sender ! {self(), ok, Message},
+      start(State)
+  end.
+
+insert_initial_data_in_finger_table(?NUMBER_OF_BITS, _, FingerTable) -> FingerTable;
+insert_initial_data_in_finger_table(LastInsertedKey, HashValue, FingerTable) ->
+  DataTuple = {HashValue, self()},
+  UpdatedFingerTable = maps:put(LastInsertedKey+1, DataTuple, FingerTable),
+  insert_initial_data_in_finger_table(LastInsertedKey+1, HashValue, UpdatedFingerTable).
+
+create_initial_finger_table(HashValue) ->
+  FingerTable = maps:new(),
+  insert_initial_data_in_finger_table(0, HashValue, FingerTable).
+
+get_hash_id(NodePID) ->
+  <<SHA256Value:256>> = crypto:hash(sha256, [NodePID]),
+  SHA256Value rem trunc(math:pow(2, ?NUMBER_OF_BITS)).
+
+init() ->
+  HashValue = get_hash_id(pid_to_list(self())),
+  FingerTable = create_initial_finger_table(HashValue),
+  State = #state{
+    id = HashValue,
+    predecessor = nil,
+    successor = self(),
+    finger_table = FingerTable
+  },
+  start(State).
--- master.erl
+++ master.erl
@@ -16,28 +16,33 @@
 -define(MASTER, master).
 -define(CHORD_NODE, chord_node).
 
-get_hash_id(Number) ->
-  NodeString = integer_to_list(Number),
-  io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, [NodeString]))]).
+add_nodes_to_network([], _) ->
+  io:fwrite("All nodes sent for addition to the network ~n");
+add_nodes_to_network([Node | Nodes], InNetworkNode) ->
+  InNetworkNode ! {self(), add_new_node, Node},
+  add_nodes_to_network(Nodes, InNetworkNode).
 
-create_nodes(0, _, _, Acc) -> Acc;
-create_nodes(NumberOfNodesYetToSpawn, TotalNumberOfNodes, TotalNumberOfRequests, Acc) ->
-  SelfHashedID = get_hash_id(NumberOfNodesYetToSpawn),
-  io:fwrite("Actor created with node value ~p and hashed valued ~p~n", [NumberOfNodesYetToSpawn, SelfHashedID]),
-  PID = spawn(?CHORD_NODE, start, []),
-  NodeTuple = {PID, SelfHashedID},
-  io:fwrite("Node Tuple created ~p~n", [NodeTuple]),
-  create_nodes(NumberOfNodesYetToSpawn-1, TotalNumberOfNodes, TotalNumberOfRequests, [NodeTuple | Acc]).
+build_p2p_network([Node | Nodes]) ->
+  add_nodes_to_network(Nodes, Node).
+
+create_nodes(0, Acc) -> Acc;
+create_nodes(NumberOfNodesYetToSpawn, Acc) ->
+  PID = spawn(?CHORD_NODE, init, []),
+  create_nodes(NumberOfNodesYetToSpawn-1, [PID | Acc]).
 
 start_master() ->
   receive
     {start_chord, NumberOfNodes, NumberOfRequests} ->
       io:fwrite("Received start chord message... Creating the nodes now ~n"),
-      create_nodes(NumberOfNodes, NumberOfNodes, NumberOfRequests, [])
+      _ = NumberOfRequests,
+      Nodes = create_nodes(NumberOfNodes, []),
+      build_p2p_network(Nodes);
+    {Sender, ok, Message} ->
+      io:fwrite("Received Message: From: ~p, Message: ~p~n", [Sender, Message])
   end,
   start_master().
 
 start(NumberOfNodes, NumberOfRequests) ->
   PID = spawn(?MODULE, start_master, []),
-  register(?MASTER, PID),
+  %register(?MASTER, PID),
   PID ! {start_chord, NumberOfNodes, NumberOfRequests}.
